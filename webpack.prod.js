10f780144e58cd54fc1f4b35d79a616468f9fa24
Richard Bray (Local) <richardobray@yahoo.co.uk>
2018-09-04 12:13:39 +0100

    moved index for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,11 @@
 const webpack = require("webpack");
+const path = require("path");
 const merge = require("webpack-merge");
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const common = require("./webpack.common");
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ROOT = path.resolve(__dirname);
 
 module.exports = merge(common, {
 	mode: "production",	
@@ -15,6 +17,9 @@ module.exports = merge(common, {
 		}),		
 		new webpack.DefinePlugin({
 			"process.env.NODE_ENV": JSON.stringify("production")
-		})
+		}),
+		new CopyWebpackPlugin([
+			{ from: `${ROOT}/index.html`, to: `${ROOT}/build/index.html` }
+		])		
 	]
 });
