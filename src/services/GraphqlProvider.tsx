8ea1984337fe7f7b26233f3e9d215adffea73200
Richard Oliver Bray <richardbray89@gmail.com>
2022-05-22 21:19:43 +0100

    chore: add error pages to gql provider

--- src/services/GraphqlProvider.tsx
+++ src/services/GraphqlProvider.tsx
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
-import { createClient, Provider, dedupExchange, fetchExchange } from 'urql';
+import { createClient, Provider, dedupExchange, fetchExchange, errorExchange } from 'urql';
 import { cacheExchange, SystemFields } from '@urql/exchange-graphcache';
 import { devtoolsExchange } from '@urql/devtools';
+import { Navigate } from "react-router-dom";
 
 import { GlobalStore } from '@services/GlobalStore';
 import Api, { Spending } from '@services/Api';
@@ -18,14 +19,15 @@ function GraphqlProvider({ children }: ReactChildren): JSX.Element | null {
   try {
     const globalStore = useContext(GlobalStore);
     const accessToken = sessionStorage.getItem('accessToken') ?? globalStore?.state.accessToken;
+    const addJWTTokenToRequests = {
+      headers: {
+        Authorization: `bearer ${accessToken}`,
+      },
+    };
 
     const client = createClient({
       url: `${API_URL}/graphql`,
-      fetchOptions: {
-        headers: {
-          Authorization: `bearer ${accessToken}`,
-        },
-      },
+      fetchOptions: addJWTTokenToRequests,
       exchanges: [
         devtoolsExchange,
         dedupExchange,
@@ -44,6 +46,16 @@ function GraphqlProvider({ children }: ReactChildren): JSX.Element | null {
               },
             },
           },
+        }),
+        errorExchange({
+          onError: (error) => {
+            const isSessionTimeoutError = error.message.includes('JsonWebTokenError');
+            if (isSessionTimeoutError) {
+              return <Navigate to="/session-error" />
+            }
+
+            return <Navigate to="/server-error" />
+          }
         }),
         fetchExchange,
       ],
