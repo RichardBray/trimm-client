75da425fe9e4b9af2d10e6093cb2a0d8f6388f38
Richard Oliver Bray <richardbray89@gmail.com>
2022-05-22 20:42:55 +0100

    chore: add basic error pages

---
+++ src/pages/Errors/GeneralError.tsx
@@ -0,0 +1,10 @@
+function GeneralError() {
+    return (
+        <>
+            <h1>Looks like something has gone wrong</h1>
+            <p>Please try to use the service again in a few minutes</p>
+        </>
+    )
+}
+
+export default GeneralError;
---
+++ src/pages/Errors/SessionError.tsx
@@ -0,0 +1,10 @@
+function SessionError() {
+    return (
+        <>
+            <h1>Your session has timed out</h1>
+            <p>Please login again to get access to the service.</p>
+        </>
+    )
+}
+
+export default SessionError;
